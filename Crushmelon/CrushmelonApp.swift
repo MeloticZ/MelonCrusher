cc10d9df2783d29459de1c14a249972f83be9c22
Korgo <hello@korgo.net>
2025-09-06 19:42:47 -0700

    Initial Commit

---
+++ Crushmelon/CrushmelonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrushmelonApp.swift
+//  Crushmelon
+//
+//  Created by Korgo on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CrushmelonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
